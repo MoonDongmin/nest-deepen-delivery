551d3e669c86fb78aeb7ec70edbff1efc04e70f1
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-07-31 12:22:17 +0900

    Payment Microservice 구현하기

--- apps/order/src/main.ts
+++ apps/order/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process    from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(process.env.DB_URL);
   await app.listen(process.env.HTTP_PORT ?? 3000);
 }
 

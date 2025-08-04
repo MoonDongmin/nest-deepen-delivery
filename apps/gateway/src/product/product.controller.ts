13cba3e0cf9ff193c2134be396e7ab6e2b9a9c95
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-08-04 12:30:07 +0900

    Product 로직 옮기기

--- apps/gateway/src/product/product.controller.ts
+++ apps/gateway/src/product/product.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Post('sample')
+  createSamples() {
+    return this.productService.createSamples();
+  }
 }

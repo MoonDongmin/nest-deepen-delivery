a7359f18b823895acc3ccc8e714f9923a913a813
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-09-15 13:12:42 +0900

    Database Port Mongoose로 변경하기

--- apps/payment/src/payment/payment.module.ts
+++ apps/payment/src/payment/payment.module.ts
@@ -6,15 +6,29 @@ import { PaymentEntity } from './adapter/output/typeorm/entity/payment.entity';
 import { TypeormAdapter } from './adapter/output/typeorm/typeorm.adapter';
 import { GrpcAdapter } from './adapter/output/grpc/grpc.adapter';
 import { PortOneAdapter } from './adapter/output/portone/portone.adapter';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  PaymentDocument,
+  PaymentSchema,
+} from './adapter/output/moongoose/document/payment.document';
+import { MongooseAdapter } from './adapter/output/moongoose/mongoose.adapter';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PaymentEntity])],
+  imports: [
+    TypeOrmModule.forFeature([PaymentEntity]),
+    MongooseModule.forFeature([
+      {
+        name: PaymentDocument.name,
+        schema: PaymentSchema,
+      },
+    ]),
+  ],
   controllers: [PaymentController],
   providers: [
     PaymentService,
     {
       provide: 'DatabaseOutputPort',
-      useClass: TypeormAdapter,
+      useClass: MongooseAdapter,
     },
     {
       provide: 'PaymentOutputPort',
